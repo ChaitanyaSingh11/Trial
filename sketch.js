ef9a88a21fec65f2221c2358a69d9bf5d03d2705
ChaitanyaSingh11 <chaitanyacoder11@gmail.com>
2020-10-31 17:34:26 +0530

    modified

--- sketch.js
+++ sketch.js
@@ -1,1 +1,20 @@
-ECHO is on.
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+let engine,world;
+
+function setup(){
+    engine = Engine.create();
+    world = engine.world;
+
+    Engine.run(engine);
+}
+
+function draw(){
+    Engine.update(engine);
+    background(221);
+    
+}
